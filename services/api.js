ac1ccb70578596bec4095ddde7f6610e8cdcf340
Rafael Paes <rfaelpaes@gmail.com>
2020-12-01 18:13:50 -0300

    feat: new API functions

--- services/api.js
+++ services/api.js
@@ -1,9 +1,27 @@
 import axios from 'axios'
 
+export const USERS = 'Users'
+
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_ENDPOINT_BASE,
 })
 
 export const searchUsersGithub = async (username) => {
-    return api.get(`/search/users?q=${username}`)
+    return await api.get(`/search/users?q=${username}&type=Users&per_page=100`)
+}
+
+export const getUserInformations = async (username) => {
+    return await api.get(`/users/${username}`)
+}
+
+export const getUserStars = async (username) => {
+    return await api.get(`/users/${username}/starred?per_page=100`)
+}
+
+export const setLocalStorageUsersItems = async (items) => {
+    return localStorage.setItem(USERS, JSON.stringify(items))
+}
+
+export const getLocalStorageUsersItems = async () => {
+    return await JSON.parse(localStorage.getItem(USERS))
 }
