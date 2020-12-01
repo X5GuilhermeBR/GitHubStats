a92ebf3c8877ddf667afad5931d3a9a6568ad679
Rafael Paes <rfaelpaes@gmail.com>
2020-12-01 18:21:04 -0300

    refactor: send user data to the profile card

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import HeadHtml from '../components/Head/index'
 import NavBar from '../components/NavBar/index'
 import SearchBar from '../components/SearchBar/index'
 import UserProfileCard from '../components/UserProfileCard'
+
+import {
+    searchUsersGithub,
+    setLocalStorageUsersItems,
+    getLocalStorageUsersItems,
+} from '../services/api'
+
 import Main from './styles'
-import HeadHtml from '../components/Head/index'
-import { searchUsersGithub } from '../services/api'
 
 const Home = () => {
     const [username, setUsername] = useState(false)
-    const [result, setResult] = useState(false)
+    const [profiles, setProfiles] = useState([])
+    const [loading, setLoading] = useState(false)
 
+    useEffect(() => {
+        getLocalStorageUsersItems()
+        .then(function(items) {
+            if (items !== null) {
+                setProfiles(items)
+                setLoading(true)
+            }
+        })
+    }, [])
+    
     const updateInputValue = (evt) => {
         setUsername(evt.target.value)
-        console.log(username)
     }
 
     const searchUser = async (evt, username) => {
         evt.preventDefault()
+        
         const { data } = await searchUsersGithub(username)
-        setResult(data.itemms)
-        console.log(data.items)
-        localStorage.setItem('Users', JSON.stringify(data.items))
+        const { items } = data
+        
+        setProfiles(items)
+        setLocalStorageUsersItems(items)
+
+        if (items) {
+            setLoading(true)
+        }
     }
 
     return (
@@ -34,25 +57,25 @@ const Home = () => {
                     username={username}
                 />
 
-                {result !== false && (
-                    <UserProfileCard
-                        name="Rafael Paes"
-                        username="@paesrafael"
-                        photo="https://avatars3.githubusercontent.com/u/46358065?s=110"
-                        followers="100 seguidores"
-                        repositories="50 repositórios"
-                        stars="20 stars"
-                        titleColor="#000000"
-                        userColor="#9B9B9B"
-                        statsColor="#000000"
-                        cardBackground="#EFEFEF"
-                        cardBoxShadow="3px 3px 6px rgba(0, 0, 0, 0.2)"
-                        cardDisplay="flex"
-                        imageBorderRadius="12px 0 0 12px"
-                        statsDisplay="flex"
-                        iconColor="#000000"
-                        iconBlock="show"
-                    />
+                {!!loading && (
+                    !!profiles && (
+                        profiles.map((item) => (
+                            <UserProfileCard
+                                key={item.id}
+                                login={item.login}
+                                titleColor="#000000"
+                                userColor="#9B9B9B"
+                                statsColor="#000000"
+                                cardBackground="#EFEFEF"
+                                cardBoxShadow="3px 3px 6px rgba(0, 0, 0, 0.2)"
+                                cardDisplay="flex"
+                                imageBorderRadius="12px 0 0 12px"
+                                statsDisplay="flex"
+                                iconColor="#000000"
+                                iconBlock="show"
+                            />
+                        ))
+                    )
                 )}
             </Main>
         </div>
