0bbc6470260650e2b6db68deb87f5458fd6e06c9
DESKTOP-6MGDPJH\gui_g <gui_guifonseca@hotmail.com>
2020-12-08 15:31:25 -0300

    Adjuste pages in next.

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container, Text } from './about/styles'
+
+export default function About() {
+    return (
+        <Container>
+            <Text>Test</Text>
+        </Container>
+    )
+}
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container, Text } from './user/profile/styles'
+
+export default function UserProfileDetails() {
+    return (
+        <Container>
+            <Text>Test</Text>
+        </Container>
+    )
+}
