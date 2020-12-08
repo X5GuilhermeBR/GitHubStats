de23d3d111d8e1ceb86e4fc872bb676ce693a8e6
DESKTOP-6MGDPJH\gui_g <gui_guifonseca@hotmail.com>
2020-12-08 14:30:33 -0300

    Adjust export const.

--- pages/user/profile/styles.js
+++ pages/user/profile/styles.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components'
 
-const Container = styled.section``
+export const Container = styled.section``
 
-const Text = styled.p``
-
-export {
-  Container,
-  Text,
-}
+export const Text = styled.p``
