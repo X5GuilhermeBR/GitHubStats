416f718efdb155e5ab3cc4e04704042371d427cc
João Victor Negreiros <joaovictornsv@gmail.com>
2020-12-09 08:31:32 -0300

    add .editorconfig

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Menu, Button } from './styles'
 
 const NavBar = () => {
-    return (
-        <Menu>
-            <ul>
-                <li>
-                    <img src={'./../../public/images/logo.png'} alt="Github Stats"/>
-                </li>
-                
-                <li>
-                    <Button className="navButton">Sobre</Button>
-                </li>
-            </ul>
-        </Menu>
-    )
+  return (
+    <Menu>
+      <ul>
+        <li>
+          <img src={'./../../public/images/logo.png'} alt="Github Stats"/>
+        </li>
+
+        <li>
+          <Button className="navButton">Sobre</Button>
+        </li>
+      </ul>
+    </Menu>
+  )
 }
 
 export default NavBar
