d84715b92dbcce277bc93a89c46c4633f9ba3ca4
Rafael Paes <rfaelpaes@gmail.com>
2020-12-10 19:36:35 -0300

    fix: search with the enter key

--- components/SearchBar/styles.js
+++ components/SearchBar/styles.js
@@ -6,45 +6,45 @@ import {
 import { MdPerson as CustomMdPerson } from 'react-icons/md'
 import { IoSearchSharp as CustomIoSearchSharp } from 'react-icons/io5'
 
-export const Div = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+export const Form = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const Input = styled(NewInput)`
-    padding: 10px;
-    width: 100%;
-    height: 55px;
-    background-color: var(--white);
+  padding: 10px;
+  width: 100%;
+  height: 55px;
+  background-color: var(--white);
 `
 
 export const Button = styled(NewButton)`
-    &.search-button {
-        padding: 10px;
-        max-width: 55px;
-        max-height: 55px;
-        border-radius: 0px 10px 10px 0px;
-        background-color: var(--white);
+  &.search-button {
+    padding: 10px;
+    max-width: 55px;
+    max-height: 55px;
+    border-radius: 0px 10px 10px 0px;
+    background-color: var(--white);
 
-        &:hover {
-            background-color: var(--hover);
-        }
+    &:hover {
+      background-color: var(--hover);
     }
+  }
 `
 
 export const MdPerson = styled(CustomMdPerson)`
-    padding-left: 10px;
-    width: 30px;
-    height: 55px;
-    color: var(--tertiary);
-    border-radius: 10px 0px 0px 10px;
-    background-color: var(--white);
+  padding-left: 10px;
+  width: 30px;
+  height: 55px;
+  color: var(--tertiary);
+  border-radius: 10px 0px 0px 10px;
+  background-color: var(--white);
 `
 
 export const IoSearchSharp = styled(CustomIoSearchSharp)`
-    width: 25px;
-    height: 50px;
-    color: var(--icon-search);
-    background-color: transparent;
+  width: 25px;
+  height: 50px;
+  color: var(--icon-search);
+  background-color: transparent;
 `
